cb4d1f61d5ae47ac7591830143a4123d230c267d
Mark Carlsen <mark@scarlsen.dk>
2024-07-08 18:04:57 +0200

    Enable and disable of field data save and loaded correctly after restart of Simhub

--- DashMenu/Settings/Settings.cs
+++ DashMenu/Settings/Settings.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -11,7 +14,7 @@ namespace DashMenu.Settings
         /// <summary>
         /// Max amount of fields that can be displayed.
         /// </summary>
-        internal int MaxFields
+        public int MaxFields
         {
             get => maxFields; set
             {
@@ -23,14 +26,47 @@ namespace DashMenu.Settings
         /// <summary>
         /// Fields displayed.
         /// </summary>
-        internal string[] DisplayedFields { get; set; }
+        public string[] DisplayedFields { get; set; }
         /// <summary>
         /// All fields. Used for enabling and disabling the fields to be able to select them.
-        /// </summary>
-        internal List<IFields> Fields { get; set; } = new List<IFields>();
+        /// </summary> 
+        [JsonConverter(typeof(IFieldConverter))]
+        public List<IFields> Fields { get; set; } = new List<IFields>();
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
+
+        public class IFieldConverter : JsonConverter
+        {
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(List<>);
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                JArray array = JArray.Load(reader);
+                var list = new List<IFields>();
+
+                foreach (JToken item in array)
+                {
+                    // Deserialize each item to Fields
+                    var fieldsObject = item.ToObject<Fields>(serializer);
+
+                    // Cast Fields to TInterface
+                    var interfaceObject = (IFields)fieldsObject;
+
+                    list.Add(interfaceObject);
+                }
+
+                return list;
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                serializer.Serialize(writer, value);
+            }
+        }
     }
 }
