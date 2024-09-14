1c0fbcdab9bd528f026b43623b9b901599108a72
Mark Carlsen <mark@scarlsen.dk>
2024-09-14 10:27:04 +0200

    Added Fuel volume field

---
+++ CommonExtensionFields/FuelVolume.cs
@@ -0,0 +1,55 @@
+﻿using DashMenu.Data;
+using GameReaderCommon;
+
+namespace CommonExtensionFields
+{
+    public class FuelVolume : FieldExtensionBase, IGaugeFieldComponent
+    {
+        public FuelVolume(string gameName) : base(gameName)
+        {
+            Data = new GaugeField()
+            {
+                Name = "Fuel",
+                IsDecimalNumber = true,
+                Decimal = 0,
+                Unit = "%",
+                Color = new ColorScheme("#ffffff", "#000000"),
+                IsRangeLocked = true,
+                Minimum = 0.ToString()
+            };
+        }
+
+        public string Description { get => "Fuel in liters or gallons depending on what Simhub is configured to display"; }
+
+        private IGaugeField data;
+        new IGaugeField Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                base.Data = value;
+            }
+        }
+
+        IDataField IDataFieldComponent.Data
+        {
+            get => Data; // Return the same GaugeField instance
+            set => Data = (IGaugeField)value; // Set the same instance
+        }
+
+        IGaugeField IGaugeFieldComponent.Data
+        {
+            get => Data;
+            set => Data = value; //Make sure to set base Data
+        }
+
+        public void Update(ref GameData data)
+        {
+            if (!data.GameRunning) return;
+            Data.Maximum = data.NewData.CarSettings.MaxFuel.ToString();
+            Data.Value = DecimalValue(data.NewData.Fuel);
+            Data.Unit = data.NewData.FuelUnit[0].ToString();
+        }
+    }
+}
