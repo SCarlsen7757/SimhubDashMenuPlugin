cc0fb493c7106045f7b17f133e6306e751c10607
Mark Carlsen <mark@scarlsen.dk>
2024-09-16 21:49:50 +0200

    Update brake bias field to guage field

--- CommonExtensionFields/BrakeBias.cs
+++ CommonExtensionFields/BrakeBias.cs
@@ -3,25 +3,45 @@ using GameReaderCommon;
 
 namespace CommonExtensionFields
 {
-    public class BrakeBias : FieldExtensionBase, IDataFieldComponent
+    public class BrakeBias : FieldExtensionBase, IGaugeFieldComponent
     {
         public BrakeBias(string gameName) : base(gameName)
         {
-            Data = new DataField()
+            Data = new GaugeField()
             {
                 Name = "BB",
                 IsDecimalNumber = true,
                 Decimal = 1,
-                Color = new ColorScheme("#d90028", "#808080")
+                Color = new ColorScheme("#d90028", "#808080"),
+                IsRangeLocked = true,
+                Maximum = 100.ToString(),
+                Minimum = 0.ToString()
             };
         }
 
         public string Description { get => "Brake bias"; }
 
+        private IGaugeField data;
+        new IGaugeField Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                base.Data = value;
+            }
+        }
+
         IDataField IDataFieldComponent.Data
         {
+            get => Data; // Return the same GaugeField instance
+            set => Data = (IGaugeField)value; // Set the same instance
+        }
+
+        IGaugeField IGaugeFieldComponent.Data
+        {
             get => Data;
-            set => Data = value;
+            set => Data = value; //Make sure to set base Data
         }
 
         public void Update(ref GameData data)
