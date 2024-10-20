6ebd7861149b8e1d5756ea43604b2e51132b8f0f
Mark Carlsen <mark@scarlsen.dk>
2024-10-20 17:30:13 +0200

    Added Alert UI

---
+++ DashMenu/UI/AlertSettingsItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DashMenu.UI
+{
+    /// <summary>
+    /// Interaction logic for AlertItem.xaml
+    /// </summary>
+    public partial class AlertSettingsItem : UserControl
+    {
+        public AlertSettingsItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DashMenu/UI/MillisecondsToTimeSpanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DashMenu.UI
+{
+    public class MillisecondsToTimeSpanConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double milliseconds)
+            {
+                return TimeSpan.FromMilliseconds(milliseconds);
+            }
+            return TimeSpan.Zero; // Default value if the input is not valid
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                return timeSpan.TotalMilliseconds; // Convert TimeSpan back to milliseconds
+            }
+            return 0; // Default value if the input is not valid
+        }
+    }
+}
