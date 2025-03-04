e13b8a4ca9583d39827921dae1f73e73ded495bf
Mark Carlsen <mark@scarlsen.dk>
2025-03-05 00:25:49 +0100

    Added air and road temp

---
+++ AccExtensionFields/AirTemperature.cs
@@ -0,0 +1,40 @@
+﻿using DashMenu.Data;
+using GameReaderCommon;
+using SimHub.Plugins;
+
+namespace AccExtensionFields
+{
+    class AirTemperature : AlertBase, IDataFieldExtension
+    {
+        public AirTemperature(string gameName) : base(gameName)
+        {
+            Data = new DataField()
+            {
+                Name = "AMB",
+                Color = new ColorScheme("#b00273")
+            };
+            Data.PropertyChanged += DataAlert_PropertyChanged;
+        }
+
+        public string Description => "Ambient Temperature.";
+
+        protected override bool GameSupported(string gameName)
+        {
+            return gameName == "AssettoCorsaCompetizione";
+        }
+
+        public override void Update(PluginManager pluginManager, ref GameData data)
+        {
+            if (!data.GameRunning) return;
+            var airTemp = data.NewData.AirTemperature;
+            if (airTemp < 0)
+            {
+                Data.Value = "-";
+                Data.Unit = string.Empty;
+                return;
+            }
+            Data.Value = airTemp.ToString();
+            Data.Unit = "°" + data.NewData.TemperatureUnit[0];
+        }
+    }
+}
---
+++ AccExtensionFields/RoadTemperature.cs
@@ -0,0 +1,40 @@
+﻿using DashMenu.Data;
+using GameReaderCommon;
+using SimHub.Plugins;
+
+namespace AccExtensionFields
+{
+    class RoadTemperature : AlertBase, IDataFieldExtension
+    {
+        public RoadTemperature(string gameName) : base(gameName)
+        {
+            Data = new DataField()
+            {
+                Name = "TST",
+                Color = new ColorScheme("#b00273")
+            };
+            Data.PropertyChanged += DataAlert_PropertyChanged;
+        }
+
+        public string Description => "Track Surface Temperature.";
+
+        protected override bool GameSupported(string gameName)
+        {
+            return gameName == "AssettoCorsaCompetizione";
+        }
+
+        public override void Update(PluginManager pluginManager, ref GameData data)
+        {
+            if (!data.GameRunning) return;
+            var roadTemp = data.NewData.RoadTemperature;
+            if (roadTemp < 0)
+            {
+                Data.Value = "-";
+                Data.Unit = string.Empty;
+                return;
+            }
+            Data.Value = roadTemp.ToString();
+            Data.Unit = "°" + data.NewData.TemperatureUnit[0];
+        }
+    }
+}
