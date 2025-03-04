83664b38eb3a353c13381361204f0e1b1ffcc4ae
Mark Carlsen <mark@scarlsen.dk>
2025-03-05 00:14:05 +0100

    Added Media info

---
+++ MediaExtensionFields/Artist.cs
@@ -0,0 +1,27 @@
+﻿using DashMenu.Data;
+using GameReaderCommon;
+using SimHub.Plugins;
+
+namespace MediaExtensionFields
+{
+    public class Artist : AlertBase, IDataFieldExtension, IAlert
+    {
+        public Artist(string gameName) : base(gameName)
+        {
+            Data = new DataField()
+            {
+                Name = "ARTIST",
+                Color = new ColorScheme("#2a6901")
+            };
+            Data.PropertyChanged += DataAlert_PropertyChanged;
+        }
+
+        public string Description => "Media Artist.";
+
+        public override void Update(PluginManager pluginManager, ref GameData data)
+        {
+            var artist = pluginManager.GetPropertyValue("MediaInfo.Artist");
+            Data.Value = artist != null ? artist.ToString() : string.Empty;
+        }
+    }
+}
---
+++ MediaExtensionFields/Title.cs
@@ -0,0 +1,27 @@
+﻿using DashMenu.Data;
+using GameReaderCommon;
+using SimHub.Plugins;
+
+namespace MediaExtensionFields
+{
+    class Title : AlertBase, IDataFieldExtension, IAlert
+    {
+        public Title(string gameName) : base(gameName)
+        {
+            Data = new DataField()
+            {
+                Name = "TITLE",
+                Color = new ColorScheme("#2a6901")
+            };
+            Data.PropertyChanged += DataAlert_PropertyChanged;
+        }
+
+        public string Description => "Media Title.";
+
+        public override void Update(PluginManager pluginManager, ref GameData data)
+        {
+            var title = pluginManager.GetPropertyValue("MediaInfo.Title");
+            Data.Value = title != null ? title.ToString() : string.Empty;
+        }
+    }
+}
